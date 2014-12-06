69275dcc2a78f49fb4c73092d9f9f767f37c3a71
Mario Pérez Esteso <mario@geekytheory.com>
2014-12-07 00:26:54 +0100

    Added files.

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.synced_folder "htdocs", "/var/www/html"
+  config.vm.provision "shell", path: "config.sh"
+end
---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##########
+### Created by Mario Pérez <mario@geekytheory.com>
+### Please, visit http://goo.gl/0mMf5Q for more info.
+##########
+# Update server
+apt-get update
+apt-get upgrade -y 
+# Install essentials
+apt-get -y install build-essential binutils-doc git -y
+# Install apache
+apt-get install apache2 -y
+#Install PHP
+apt-get install php5 libapache2-mod-php5 php5-cli php5-mysql -y
+# Install MySQL
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get install mysql-client mysql-server -y
+# Install PhpMyAdmin
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password root' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+apt-get install phpmyadmin -y
+# Restart Apache service
+service apache2 restart
